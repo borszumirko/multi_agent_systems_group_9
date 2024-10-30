5f255abd25ffd6df5b7139b87bac1908de9381b2
housemountainer <hausihaus97@gmail.com>
2024-10-30 15:03:01 +0100

    Added documentation

--- main.py
+++ main.py
@@ -5,15 +5,28 @@ import numpy as np
 import random
 from constants import CSV_FILE_NAME
 
-def set_seed(seed: int):
+def set_seed(seed: int) -> None:
+    """
+    Sets the random seed for both numpy and Python's built-in random generators.
+
+    Parameters:
+        seed (int): The seed value for reproducibility.
+    """
     np.random.seed(seed)          # For numpy's RNG
     random.seed(seed)             # For Python's built-in RNG
 
-def main():
+def main() -> None:
+    """Runs a single simulation loop."""
     simulation = Simulation()
     simulation.main_loop()
 
-def n_runs(n=30):
+def n_runs(n:int=30) -> None:
+    """
+    Executes multiple simulation runs and saves each run's results in separate files.
+
+    Parameters:
+        n (int): The number of simulation runs. Default is 30.
+    """
     for i in range(n):
         name = CSV_FILE_NAME[:-4] + f"_subrun_{i}.csv"
         simulation = Simulation(name, False)
@@ -21,9 +34,13 @@ def n_runs(n=30):
 
 import random
 if __name__=="__main__":
-    #main()
-    set_seed(42)
-    n_runs(2)
+    set_seed(42)  # Set seed for reproducibility
+
+    # Uncomment to run a single simulation loop
+    # main()
+    
+    n_runs(2)     # Run several simulations
+
     #cProfile.run('main()', 'profiling_stats')
     #p = pstats.Stats('profiling_stats')
     #p.sort_stats(pstats.SortKey.TIME).print_stats(10)  # Adjust the number to see more or fewer results
